a1fd23363752bf28de82305f55fcc572455f035c
Ebuks <ebukaa@protonmail.com>
2022-07-16 23:05:33 +0100

    modified files

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,5 @@ module.exports = (api) => {
   api.cache(true);
   return {
     presets: [["babel-preset-expo", { lazyImports: true }]],
-    plugins: [["@babel/plugin-transform-react-jsx", { runtime: "automatic" }]],
   };
 };
--- screens/home.js
+++ screens/home.js
@@ -1,3 +1,14 @@
-export function useHome() {
-  return <></>;
+import { lazy, Suspense } from "react";
+import { View } from "react-native";
+
+const Bar = lazy(() => import("components/bar"));
+
+export default function Home() {
+  return (
+    <View>
+      <Suspense fallback={null}>
+        <Bar />
+      </Suspense>
+    </View>
+  );
 }
