152b5b531918b12563c66956a956627c49824e95
Ebuks <ebukaa@protonmail.com>
2022-12-18 23:57:53 +0000

    Renamed function to use hook

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 import Google from "assets/google.svg";
 import classNames from "./style.module.scss";
 
-export default function Home() {
+export default function useHome() {
   return (
     <View className={classNames.home}>
       <Text className={classNames.text}>Home</Text>
