d2ee4a3b6d1ec5b937988388ad989dea9ec0dcc8
Ebuks <ebukaa@protonmail.com>
2022-12-18 13:12:16 +0000

    Added & styled Google icon

--- screens/home/index.tsx
+++ screens/home/index.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
+import Google from "assets/google.svg";
 import classNames from "./style.module.scss";
 
 export default function Home() {
   return (
     <View className={classNames.home}>
       <Text className={classNames.text}>Home</Text>
+      <Google className={classNames.google} />
     </View>
   );
 }
