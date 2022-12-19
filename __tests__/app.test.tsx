5669465e0a7eafcb806712c2ca671e8593fde3da
Ebuks <ebukaa@protonmail.com>
2022-12-19 12:25:04 +0000

    Removed jest.genMockFromModule("components")

--- __tests__/app.test.tsx
+++ __tests__/app.test.tsx
@@ -2,7 +2,6 @@ import { render } from "@testing-library/react-native";
 import type { ReactTestRendererJSON } from "react-test-renderer";
 import App from "App";
 
-jest.genMockFromModule("components");
 jest.mock("components");
 
 describe("App", () => {
