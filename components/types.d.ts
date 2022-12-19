027a00049d346326aea50597446dad35460a7a9a
Ebuks <ebukaa@protonmail.com>
2022-12-19 10:02:40 +0000

    More types & replaced Optional with Partial type

--- components/types.d.ts
+++ components/types.d.ts
@@ -1,16 +1,17 @@
 import type { ParamListBase } from "@react-navigation/native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export type StackScreenProps = NativeStackScreenProps<ParamListBase>;
+export type ComponentType = object & { name: string };
+
+export type Action = "get" | "set" | "access";
+
+export type StackScreenProps = Partial<NativeStackScreenProps<ParamListBase>>;
 
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
-export type Optional<Type, Key extends keyof Type> = Omit<Type, Key> &
-  Partial<Pick<Type, Key>>;
-
 export type Properties<Component extends object> = {
-  [property in keyof Optional<Component, keyof Component>]: {
+  [property in keyof Partial<Component>]: {
     value?: Component[property];
     get?: () => Component[property];
     set?: (value: Component[property]) => void;
