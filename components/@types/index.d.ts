de83f5b9cb2a87346c7adfd755768759b0cd15cf
Ebuks <ebukaa@protonmail.com>
2022-12-27 11:43:50 +0000

    Removed unnecessary types

--- components/@types/index.d.ts
+++ components/@types/index.d.ts
@@ -1,25 +1,7 @@
 import type { ParamListBase } from "@react-navigation/native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 
-export type ComponentType = object & { name: string };
-
-export type Action = "get" | "set" | "access";
-
 export type StackScreenProps = Partial<NativeStackScreenProps<ParamListBase>>;
 
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
-
-export type Properties<Component extends object> = {
-  [property in keyof Partial<Component>]: {
-    value?: Component[property];
-    get?: () => Component[property];
-    set?: (value: Component[property]) => void;
-    writable?: boolean;
-    configurable?: boolean;
-  };
-};
-
-interface ImportedComponent<Props extends object> {
-  default(props: Props | object): JSX.Element | null;
-}
