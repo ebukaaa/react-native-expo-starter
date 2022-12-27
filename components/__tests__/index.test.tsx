f8f31206bb3fb0a9f5357601d403683b570db1fe
Ebuks <ebukaa@protonmail.com>
2022-12-27 11:43:19 +0000

    Tested if load() get called

--- components/__tests__/index.test.tsx
+++ components/__tests__/index.test.tsx
@@ -1,8 +1,12 @@
 import { render } from "@testing-library/react-native";
 import { Home } from "components";
+import { load } from "@pascinso/utils";
+
+jest.mock("@pascinso/utils", () => ({ load: jest.fn() }));
 
 describe("Home", () => {
   it("should load component", () => {
     expect(render(<Home />).container).toBeTruthy();
+    expect(load).toHaveBeenCalledTimes(1);
   });
 });
