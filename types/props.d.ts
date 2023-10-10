536a4da583cd286b39e724bf2fdc93dbc04118c8
Ebuka Isiadinso <ebukaa@proton.me>
2023-10-10 20:00:07 +0100

    Exported props type w/ ReactNative declarations

---
+++ types/props.d.ts
@@ -0,0 +1,17 @@
+export interface Props
+  extends Partial<{
+    styleName: string | undefined;
+    className: Record<string, string>;
+  }> {}
+
+declare module "react-native" {
+  interface ViewProps extends Props {}
+
+  interface TextProps extends Props {}
+
+  interface ScrollViewProps extends Props {}
+
+  interface ImageBackgroundProps extends Props {}
+
+  interface TouchableOpacityProps extends Props {}
+}
