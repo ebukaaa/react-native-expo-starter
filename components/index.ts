e0344b8cc0665a188e7d2430046ee4ad9d1bd506
Ebuka Isiadinso <ebukaa@proton.me>
2023-10-10 19:06:37 +0100

    Updated components/hooks/icons index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,17 @@
-import { load } from "@pascinso/utils";
-import type { StackScreenProps } from "./@types";
+export {
+  View,
+  Text,
+  Alert,
+  LogBox,
+  FlatList,
+  Image as Picture,
+  TextInput,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
-export const Home = (props: StackScreenProps) =>
-  load(import("screens/home"), props);
+export { Link, Tabs, Stack, ErrorBoundary } from "expo-router";
 
-export default Home;
+export { ScrollViewStyleReset } from "expo-router/html";
+
+export { Image } from "expo-image";
