0b1f6aa3185cc7d5a67dd0b5ddd934f0d11025ca
Ebuka Isiadinso <ebukaa@proton.me>
2023-11-26 17:11:58 +0000

    Removed useless import types & extracted stack/route types

--- types/navigation.d.ts
+++ types/navigation.d.ts
@@ -1,23 +1,26 @@
-import type { NavigationProp } from "types";
+type Stacks = "(home)" | "about";
 
-interface StackParams {
-  Home: undefined;
-  About: undefined;
-}
+interface StackParams extends Record<Stacks, undefined> {}
+
+type RouteProp<T> = import("types").RouteProp<T, keyof T>;
+
+interface Route extends Readonly<RouteProp<StackParams>> {}
 
 interface Stack
-  extends NavigationProp<
-    StackParams,
-    keyof StackParams,
-    undefined,
-    Readonly<
-      Record<"key" | "type", string> & {
-        index: number;
-        routeNames: (keyof StackParams)[];
-        history?: unknown[];
-        routes: import("types").RouteProp<StackParams, keyof StackParams>[];
-        stale: false;
-      }
-    >,
-    import("types").NativeStackNavigationOptions
+  extends Readonly<
+    import("types").NavigationProp<
+      StackParams,
+      keyof StackParams,
+      undefined,
+      Readonly<
+        Record<"key" | "type", string> & {
+          index: number;
+          routeNames: (keyof StackParams)[];
+          history?: unknown[];
+          routes: RouteProp<StackParams>[];
+          stale: false;
+        }
+      >,
+      import("types").NativeStackNavigationOptions
+    >
   > {}
