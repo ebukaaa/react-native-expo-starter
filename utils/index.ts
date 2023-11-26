a3d111ad3446786874c10a1131def9614a699268
Ebuka Isiadinso <ebukaa@proton.me>
2023-11-26 17:09:44 +0000

    Exported load() & get-dimension utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,5 @@
-import { Dimensions } from "react-native";
-
-export const { width: vw, height: vh } = Dimensions.get("screen");
-
 export { Platform } from "react-native";
 
-export { defineProperties } from "@pascinso/utils";
+export { defineProperties, load } from "@pascinso/utils";
+
+export * from "./get-dimension";
