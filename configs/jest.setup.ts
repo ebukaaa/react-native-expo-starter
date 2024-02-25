b11321dd0fe62298e9b7f23ba31b6444a08626c2
Ebuka Isiadinso <39866607+pascinso@users.noreply.github.com>
2024-02-25 08:39:17 +0000

    Removed imports

--- configs/jest.setup.ts
+++ configs/jest.setup.ts
@@ -1,5 +1,2 @@
-import "react-native-gesture-handler/jestSetup";
-import "react-native-reanimated/mock";
-
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 jest.mock("*.svg", () => jest.fn());
