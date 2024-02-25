53d40367f0f431db1dcc826618e81d431154de2b
Ebuka Isiadinso <39866607+pascinso@users.noreply.github.com>
2024-02-25 15:01:36 +0000

    Mocked funcs from @pascinso/utils, expo-router/**, hooks & **/logbox/**

--- configs/jest.setup.ts
+++ configs/jest.setup.ts
@@ -1,2 +1,19 @@
 jest.mock("react-native/Libraries/Animated/NativeAnimatedHelper");
 jest.mock("*.svg", () => jest.fn());
+jest.mock("utils/@pascinso", () => ({
+  load: jest.fn(),
+  defineProperties: jest.fn(),
+}));
+jest.mock("expo-router", () => ({
+  Stack: { Screen: jest.fn() },
+  Link: ({ children }) => children,
+}));
+
+jest.mock("expo-router/html", () => ({ ScrollViewStyleReset: jest.fn() }));
+jest.mock("hooks", () => ({
+  useNavigation: () => ({ setOptions: jest.fn() }),
+  useLayoutEffect: jest.fn(),
+}));
+jest.mock("components/mocks/logbox", () => ({
+  LogBox: { ignoreLogs: jest.fn() },
+}));
