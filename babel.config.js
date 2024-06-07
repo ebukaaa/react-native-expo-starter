bc4f8c9d06864ef240fcb3ab683d290687bf3b5e
Ebuka Isiadinso <39866607+pascinso@users.noreply.github.com>
2024-06-07 12:37:34 +0100

    Added babel configs types & removed dotenv plugins rules

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,6 @@
+/** @type {import('@babel/core').ConfigFunction} */
+
 module.exports = function useConfig(api) {
   api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-          blacklist: null,
-          whitelist: null,
-          safe: false,
-          allowUndefined: true,
-        },
-      ],
-    ],
-  };
+  return { presets: ["babel-preset-expo"] };
 };
